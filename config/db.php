12c80c2eb44381ded4032d5e1da2982420463cbe
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2023-05-14 15:17:16 +0300

    Настроил подключение к БД

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=mysql;dbname=mydb',
     'username' => 'root',
-    'password' => '',
+    'password' => 'secret',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
