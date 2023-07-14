208f951705861cbf9999b13bdf8df13105ba38e9
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2023-07-14 12:59:19 +0300

    Добавлены ссылки в navbar. Убраны лишние

--- views/authors/index.php
+++ views/authors/index.php
@@ -40,35 +40,4 @@ $this->params['breadcrumbs'][] = $this->title;
         </tr>
         </tbody>
     </table>
-    <!--<!-- Create Author Modal -->-->
-    <!--<div class="modal fade" id="authorCreateModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">-->
-    <!--    <div class="modal-dialog">-->
-    <!--        <div class="modal-content">-->
-    <!--            <div class="modal-header">-->
-    <!--                <h5 class="modal-title" id="exampleModalLabel">Create new author</h5>-->
-    <!--                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>-->
-    <!--            </div>-->
-    <!--            <div class="modal-body">-->
-    <!--                <form>-->
-    <!--                    <div class="form-group row">-->
-    <!--                        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>-->
-    <!--                        <div class="col-sm-10">-->
-    <!--                            <input type="text" class="form-control form-control-sm" id="staticEmail" value="user@example.com">-->
-    <!--                        </div>-->
-    <!--                    </div>-->
-    <!--                    <div class="form-group row">-->
-    <!--                        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>-->
-    <!--                        <div class="col-sm-10">-->
-    <!--                            <input type="password" class="form-control form-control-sm" id="inputPassword" value="password">-->
-    <!--                        </div>-->
-    <!--                    </div>-->
-    <!--                </form>-->
-    <!--            </div>-->
-    <!--            <div class="modal-footer">-->
-    <!--                <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="modal">Close</button>-->
-    <!--                <button type="button" class="btn btn-primary btn-sm">Save changes</button>-->
-    <!--            </div>-->
-    <!--        </div>-->
-    <!--    </div>-->
-    <!--</div>-->
-<?php $this->registerJsFile('@web/js/author.js')?>
+<?php //$this->registerJsFile('@web/js/author.js')?>
---
+++ views/site/home.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent">
+        <h1 class="display-6">Hi!</h1>
+
+        <p class="lead">To open CRUD click the button under</p>
+
+        <p><a class="btn btn-lg btn-success" href="/authors">Get started CRUD</a></p>
+    </div>
+
+    <div class="body-content">
+        <div class="row">
+        </div>
+    </div>
+</div>
